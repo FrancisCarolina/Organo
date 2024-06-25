d56d5baa4855b4e88284a28d67c03f39a95342fb
Francis Lovo <francis.carolinalovo@gmail.com>
2024-06-25 18:46:59 -0300

    cadastrando colaborador ao array de colaboradores

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -4,7 +4,7 @@ import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
 
-const Formulario = () => {
+const Formulario = (props) => {
   const times = [
     "Programação",
     "Front-End",
@@ -22,7 +22,7 @@ const Formulario = () => {
 
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log("Form: ", nome, cargo, imagem, time);
+    props.aoColaboradorCadastrado({ nome, cargo, imagem, time });
   };
 
   return (
