0e8364ac2125f05fc363e7d58398a3d2ff561517
Francis Lovo <francis.carolinalovo@gmail.com>
2024-06-28 12:40:38 -0300

    card de colaborador

---
+++ organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,20 @@
+import "./Colaborador.css";
+
+const Colaborador = (props) => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img
+          src="https://github.com/FrancisCarolina.png"
+          alt="Francis Carolina"
+        />
+      </div>
+      <div className="rodape">
+        <h4>Francis Carolina</h4>
+        <h5>Professora</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
