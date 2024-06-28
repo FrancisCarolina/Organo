254b747bb417ae2de4181ab78652dc31f33848b8
Francis Lovo <francis.carolinalovo@gmail.com>
2024-06-28 12:57:20 -0300

    escondendo times vazios

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -3,14 +3,19 @@ import "./Time.css";
 
 const Time = (props) => {
   return (
-    <section className="time" style={{ backgroundColor: props.corSecundaria }}>
-      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-      <div className="colaboradores">
-        {props.colaboradores.map((colaborador) => (
-          <Colaborador colaborador={colaborador} />
-        ))}
-      </div>
-    </section>
+    props.colaboradores.length > 0 && (
+      <section
+        className="time"
+        style={{ backgroundColor: props.corSecundaria }}
+      >
+        <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        <div className="colaboradores">
+          {props.colaboradores.map((colaborador) => (
+            <Colaborador colaborador={colaborador} />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 export default Time;
