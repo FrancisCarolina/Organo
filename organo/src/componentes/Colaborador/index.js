2a166c6c49d7040b97391afbe55fe8a905780ba5
Francis Lovo <francis.carolinalovo@gmail.com>
2024-06-28 12:52:20 -0300

    mapeando colaboradores

--- organo/src/componentes/Colaborador/index.js
+++ organo/src/componentes/Colaborador/index.js
@@ -1,17 +1,14 @@
 import "./Colaborador.css";
 
-const Colaborador = (props) => {
+const Colaborador = ({ colaborador }) => {
   return (
     <div className="colaborador">
       <div className="cabecalho">
-        <img
-          src="https://github.com/FrancisCarolina.png"
-          alt="Francis Carolina"
-        />
+        <img src={colaborador.imagem} alt={colaborador.nome} />
       </div>
       <div className="rodape">
-        <h4>Francis Carolina</h4>
-        <h5>Professora</h5>
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
       </div>
     </div>
   );
--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -5,8 +5,11 @@ const Time = (props) => {
   return (
     <section className="time" style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-      <Colaborador />
-      <Colaborador />
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador colaborador={colaborador} />
+        ))}
+      </div>
     </section>
   );
 };
